71e44f5c240236fe788b28a98137134a8af8a003
YIFA233 <112579794+YIFA233@users.noreply.github.com>
2024-10-02 18:21:47 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory, make_response, jsonify
-from db import get_collection
+from flask import Flask, request, render_template, send_from_directory, make_response
 
 app = Flask(__name__)
 
@@ -9,6 +8,11 @@ app = Flask(__name__)
 @app.route('/') #root route
 def home():
     response = make_response(render_template('index.html'))
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+@app.after_request
+def secure_header(response):
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
 
@@ -29,37 +33,6 @@ def serve_images(filename):
 @app.route('/test') # for test purpose only, Modify this if you have any to try out
 def hello_world():
     return '<h1>Hello!</h1>'
-
-# add test data to MongoDB
-@app.route('/add_test_data', methods=['GET'])
-def add_test_data():
-    # test data
-    test_data = {"name": "Test User", "email": "testuser@example.com"}
-
-    # use get_collection to get the collection
-    collection = get_collection('test_collection')
-
-    # insert data into MongoDB
-    collection.insert_one(test_data)
-
-    return "Test data added successfully!"
-
-# get test data from MongoDB
-@app.route('/get_test_data', methods=['GET'])
-def get_test_data():
-    collection = get_collection('test_collection')
-
-    # get all data from the collection
-    data = collection.find()
-    
-    # convert data to list for JSON serialization
-    data_list = []
-    for item in data:
-        # convert ObjectId to string
-        item['_id'] = str(item['_id'])
-        data_list.append(item)
-
-    return jsonify(data_list)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
