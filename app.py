b1f22d19e4b5421eb1e4c6af546596fcf2ee52a5
theGregGwalker <w735489375@163.com>
2024-10-03 20:28:50 -0400

    Restructured static folder. Modified app.py. Now each file has correct mime type and nosniff

--- app.py
+++ app.py
@@ -11,11 +11,6 @@ def home():
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
 
-@app.after_request
-def secure_header(response):
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
 @app.route('/login')
 def login():
     return "Modify your login page here"
@@ -25,15 +20,34 @@ def page_not_found(e):
     response = make_response(render_template('404.html'), 404)
     return response
 
-@app.route('/static/images/<path:filename>')
-def serve_images(filename):
-    response = send_from_directory('static/images', filename)
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
+@app.route('/static/css/<path:filename>')
+def serve_css(filename):
+    response = send_from_directory('static/css', filename)
+    return response, 200, {'Content-Type': 'text/css; charset=utf-8'}
 
+@app.route('/static/js/<path:filename>')
+def serve_js(filename):
+    response = send_from_directory('static/js', filename)
+    return response, 200, {'Content-Type': 'text/javascript; charset=utf-8'}
+
+@app.route('/static/images/<path:filename>')
+def serve_image(filename):
+    response = send_from_directory('static/images', filename)
+    return response, 200, {'Content-Type': 'image/jpeg'}
+# ---------------------------Test Route---------------------------
 @app.route('/test') # for test purpose only, Modify this if you have any to try out
 def hello_world():
     return '<h1>Hello!</h1>'
+# ---------------------------After Request---------------------------
+@app.after_request
+def secure_header(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    # url = request.url
+    # if url.endswith('.css'):
+    #     response.headers['Content-Type'] = 'text/css'
+    # elif url.endswith('.js'):
+    #     response.headers['Content-Type'] = 'text/javascript'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- static/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('login-icon').addEventListener('click', function(event) {
-    event.preventDefault();
-
-    window.location.href = '/login'; // Redirect to login page
-});
-document.getElementById('chat-icon').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = '/chat'; // Redirect to chat page
-});
