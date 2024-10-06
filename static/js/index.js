1e4a3a059a669f18d2d50d98e6a859aa662a4af5
kaifengli <li.kaifeng@outlook.com>
2024-10-06 03:22:26 -0400

    Add like buttons to each post

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,8 @@
-document.getElementById('login-icon').addEventListener('click', function(event) {
+document.getElementById('login-icon').addEventListener('click', function (event) {
     event.preventDefault();
-
     window.location.href = '/login'; // Redirect to login page
 });
-document.getElementById('chat-icon').addEventListener('click', function(event) {
+document.getElementById('chat-icon').addEventListener('click', function (event) {
     event.preventDefault();
     window.location.href = '/chat'; // Redirect to chat page
 });
