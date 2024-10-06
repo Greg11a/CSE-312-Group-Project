2283bb2300872249a2a2648476b34f3d55442bae
Kaifeng Li <li.kaifeng@outlook.com>
2024-10-06 17:38:47 -0400

    Make like button clickable

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,21 @@
+// Make like button clickable
+document.addEventListener('DOMContentLoaded', () => {
+    const likeButtons = document.querySelectorAll('.like-button');
+
+    likeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const isLiked = button.getAttribute('liked') === 'true';
+            button.setAttribute('liked', !isLiked);
+            const icon = button.querySelector('.material-icons');
+            if (isLiked) {
+                icon.textContent = 'thumb_up_off_alt';
+            } else {
+                icon.textContent = 'thumb_up';
+            }
+        });
+    });
+});
+
 document.getElementById('login-icon').addEventListener('click', function (event) {
     event.preventDefault();
     window.location.href = '/login'; // Redirect to login page
